46105a7435e33f8a0e89e48597365052d8f45ccd
NaveenNeerudi475 <naveenneerudi1475@gmail.com>
2026-03-04 22:04:04 +0530

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,5 +1,5 @@
 
-sum=0
+total_sum = 0
 for i in range(1,51):
-    sum+=i
-print(f"the sum of numbers from 1 to 50 is : {sum}")
+    total_sum + =i
+print(f"the sum of numbers from 1 to 50 is : {total_sum}")
